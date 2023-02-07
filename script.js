eec093ed9808a8081fa0556068bce67fc063449b
David Keim <dmk252@cornell.edu>
2023-02-07 15:41:36 -0500

    added reference values for constructing the password

--- script.js
+++ script.js
@@ -53,7 +53,10 @@ function generatePassword() {
   //set up initial output variable
   var passwordOutput = "";
   //set up reference list of characters from which random elements can be drawn to build the password
-  
+  let refLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let refUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let refNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  let refSpecChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ".", ",", "/", "?", "[", "]", "<", ">"];
 }
 
 // Add event listener to generate button
